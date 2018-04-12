619f06c812c29e310fa8e4741f47805010932247
admin <admin@Xiaojuans-MacBook-Pro.local>
2018-04-12 10:54:31 +0200

    look at results

--- final_stage/views.py
+++ final_stage/views.py
@@ -25,12 +25,11 @@ def FinalStage(request, game_id, product_id):
     images = [shoe.image, Shoe.objects.get(id=game.target_image_id).image]
     return render(request, 'final_stage/description_diff.html', locals())
 
-def Description(request, game_id, product_id):
+def Description(request, game_id):
 
     game = Game.objects.get(id=game_id)
-    shoe = OneImage.objects.get(id = product_id)
+    shoe = OneImage.objects.get(id = game.point)
 
-    game.point = product_id
     print("SHOE = ", shoe, game.user_id, game.target_image_id)
 
     initial_description = ShoeDescriptionByAssessor.objects.get(user_profile=game.user_id,
