e01deff75a1007fcd5a0efde7e1c3294cf991ad3
admin <admin@Xiaojuans-MacBook-Pro.local>
2018-04-14 23:53:26 +0200

    copy of logins

--- final_stage/forms.py
+++ final_stage/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import *
 from shoes.forms import ShoeDescriptionByAssessor, Shoe
 from search.forms import OneImage, Game
+from landing.forms import Login, AssessorProfile
 
 
 
--- final_stage/views.py
+++ final_stage/views.py
@@ -1,10 +1,42 @@
 from django.shortcuts import render, HttpResponseRedirect
-from .forms import OneImage, Game, Shoe, ShoeDescriptionByAssessor
+from .forms import OneImage, Game, Shoe, ShoeDescriptionByAssessor, Login, AssessorProfile
 import os
 import numpy as np
 #from products.models import *
+def Copy_logins_and_Profiles(path_login, path_user_profile):
+    logins = Login.objects.all()
+    with open(path_login, 'w') as login_file, open(path_user_profile, 'w') as user_file:
+        for login in logins:
+            login_file.write(str(login.id) + "\t" + str(login.user_name) + "\t" +
+                             str(login.password) + "\n")
+            if (AssessorProfile.objects.filter(login=login.id).exists()):
+                a_p = AssessorProfile.objects.get(login=login.id)
+                user_file.write(str(a_p.id) + "\t" + str(a_p.login) + "\t" + str(a_p.carrer_feild) +
+                                "\t" + str(a_p.gender) + "\t" + str(a_p.age) + "\t" +
+                                str(a_p.expirience_in_online_shopping) + "\n")
+
+def CreateProfilesFromCopy(path_login, path_user_profile):
+    logins = open(path_login).readlines()
+    profiles = open(path_user_profile).readlines()
+    profiles_dic = {}
+    for profile in profiles:
+        profile = profile.strip().split("\t")
+        profiles_dic[profile[1]] = profile[2:]
+
+    for login in logins:
+        login = login.strip().split("\t")
+        log = Login.objects.create(user_name = login[1], password=login[2])
+        if login[0] in profiles_dic:
+            profile = profiles_dic[login[0]]
+            print profile
+            AssessorProfile.objects.create(login=log.id, carrer_feild=profile[0],
+                                       gender=profile[1],
+                                       age=profile[2],
+                                       expirience_in_online_shopping=profile[3])
 
 def Statistics(request):
+    #Copy_logins_and_Profiles("static/text_files/logins.txt", "static/text_files/user_profile.txt")
+    #CreateProfilesFromCopy("static/text_files/logins.txt", "static/text_files/user_profile.txt")
     games = Game.objects.all()
     games_sucsess = [game for game in games if game.sucsess==1]
     games_fall = [game for game in games if game.sucsess == -1]
